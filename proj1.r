d81e8443f6d209fb6967dc5488d5ae9b8ed578ae
Mengyao41 <s2049018@ed.ac.uk>
2022-09-29 15:39:09 +0100

    Original file

---
+++ proj1.r
@@ -0,0 +1,5 @@
+setwd("C:/Users/surface/Documents/Rscripts/Groupwork18")
+a <- scan("pg10.txt",what="character",skip=104) ## skip contents
+n <- length(a)
+a <- a[-((n-2886):n)] ## strip license
+a <- a[-grep("[0123456789]:[0123456789]",a)] ## strip out verse numbers

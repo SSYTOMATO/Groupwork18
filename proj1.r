1ad24023a45b910ba17a474d9185433cba9376bf
Mengyao41 <s2049018@ed.ac.uk>
2022-09-29 16:59:59 +0100

    find ?

--- proj1.r
+++ proj1.r
@@ -3,3 +3,38 @@ a <- scan("pg10.txt",what="character",skip=104) ## skip contents
 n <- length(a)
 a <- a[-((n-2886):n)] ## strip license
 a <- a[-grep("[0123456789]:[0123456789]",a)] ## strip out verse numbers
+
+###comments
+
+
+# step 4
+bible <- strsplit(a," ") # a vector of original bible text
+
+# the split_punct function with the parameters of a word_vector and a punctuation vector
+# to search for each word containing the punctuation mark, remove it from the word
+# and add the mark as a new entry in the vector of words, after the word it came from
+# note that "." is special punctuation, so we use if function to deal with it.
+split_punct <- function(word_vector, punct_vector){
+  for (punct in punct_vector){
+    ii <- grep(punct, word_vector, fixed = TRUE)
+    new_word <- rep("", length(ii) + length(word_vector))
+    iis <- ii + 1:length(ii)
+    new_word[iis] <- punct
+    if (punct == "."){
+      word_vector <- gsub("\\.","", word_vector)
+    }else{
+    word_vector <- gsub(punct, "", word_vector)
+    }
+    new_word[-iis] <- word_vector
+    word_vector <- new_word
+  }
+  word_vector
+}
+
+# step 5
+bible_punct <- c(",",".", ";", "!", ":", "?") # a vector of punctuations in bible
+new_bible <- split_punct(bible, bible_punct) # bible text with splitted punctuations
+
+# step 6
+lower_bible <- tolower(new_bible) # bible vector with lower case letters
+b <- unique(lower_bible)

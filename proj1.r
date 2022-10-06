adbcf1f1d64774534eb442f8b65b5b54c706af45
Mengyao41 <s2049018@ed.ac.uk>
2022-10-06 15:27:59 +0100

    change some comments

--- proj1.r
+++ proj1.r
@@ -6,70 +6,86 @@
 # Each of us double-checked and optimized others' codes.  
 # Everyone undertakes roughly 1/3 of the work.
 
-setwd("C:/Users/surface/Documents/Rscripts/Groupwork18")
-a <- scan("pg10.txt",what="character",skip=104) ## skip contents
+#-------------------------------------------------------------------------------
+
+# step 3
+
+setwd("C:/Users/surface/Documents/Rscripts/Groupwork18") # set the working directory
+a <- scan("pg10.txt",what="character",skip=104) # skip contents
 n <- length(a)
-a <- a[-((n-2886):n)] ## strip license
-a <- a[-grep("[0123456789]:[0123456789]",a)] ## strip out verse numbers
+a <- a[-((n-2886):n)] # strip license
+a <- a[-grep("[0123456789]:[0123456789]",a)] # strip out verse numbers
+
+#-------------------------------------------------------------------------------
 
 # step 4
-bible <- strsplit(a," ") # a vector of original bible text
 
-# After splite the original text into individual words, 
-# A loop is used to traverse the text to find the location of specific punctuation marks, 
-# remove these symbols from the original text and 
-# place them back into the vector of words as new individual items.
-# And name the procedure split_punct function.
+bible <- strsplit(a," ") # splite the original text into individual words
+
+# a function to find the location of specific punctuation marks, remove them from the original text and
+# add the mark as a new entry in the vector of words, after the word it came from
 split_punct <- function(word_vector, punct_vector){
+  
   for (punct in punct_vector){
-    ii <- grep(punct, word_vector, fixed = TRUE) # Use grep to find the position indes of specific punctuation marks
-    new_word <- rep("", length(ii) + length(word_vector))
-    iis <- ii + 1:length(ii) # Decision the position index of specific punctuation marks, who will be placed back into the vector of new_words as new individual items.
-    new_word[iis] <- punct # Placed back all specific punctuation marks into the vector of new_words.
+    ii <- grep(punct, word_vector, fixed = TRUE) # indices of the punctuation in word_vector
+    new_word <- rep("", length(ii) + length(word_vector)) # vector to store both words and punctuations
+    iis <- ii + 1:length(ii) # decide where should punctuations go in new_word
+    new_word[iis] <- punct # insert punctuations
+    
+    # handle full stop and other punctuation marks separately
     if (punct == "."){
-      word_vector <- gsub("\\.","", word_vector)#Using the gsub function, "." is removed from items containing "." in the original text. As the "\\" needs to be added before "."  in order to be accurately identified by gsub function, we handle this separately.
+      word_vector <- gsub("\\.","", word_vector) # remove full stops from word_vector
     }else{
-      word_vector <- gsub(punct, "", word_vector,fixed = TRUE)# Using the gsub function to remove all other specific punctuation marks from original text.
+      word_vector <- gsub(punct, "", word_vector,fixed = TRUE) # remove other punctuation marks
     }
-    new_word[-iis] <- word_vector #Put all the items, except the specific punctuation marks, into the remaining positions in the new vector.
-    word_vector <- new_word
+    new_word[-iis] <- word_vector # insert words without punctuations
+    word_vector <- new_word # update word_vector for the next round
   }
   word_vector
 }
 
-# step 5
-# The bible text is processed using the split_punct function.
-bible_punct <- c(",",".", ";", "!", ":", "?") # A vector of punctuations in bible
-a_with_capital <- split_punct(bible, bible_punct) # Bible text with splitted punctuations
+#-------------------------------------------------------------------------------
 
+# step 5
+
+# use the split_punct function to handle the original bible text
+bible_punct <- c(",",".", ";", "!", ":", "?") # a vector of punctuations in bible
+a_with_capital <- split_punct(bible, bible_punct) # bible text with splitted punctuations
+
+#-------------------------------------------------------------------------------
 
 # step 6
 # In this step, we want to find a vector containing about 500 most common words.
-a <- tolower(a_with_capital) # bible vector with lower case letters
-b <- unique(a) # find the vector of unique words
-index <- match(a,b) # find the vector of indicies indicating which element in b each element in a corresponds to
 
-freq_vector <- c(tabulate(index)) # find the frequency of each element of b in a 
-freq_sort <- sort(freq_vector, decreasing = TRUE) # use sort function to get a decreasingly ordered frequency vector
+a <- tolower(a_with_capital) # bible vector with lower case letters
+b <- unique(a) # the vector of unique words
+index <- match(a,b) # indicies indicating which element in b each element in a corresponds to
+
+freq_vector <- c(tabulate(index)) # the frequency of b's elements in a
+freq_sort <- sort(freq_vector, decreasing = TRUE) # a decreasingly ordered frequency vector
 
 # Now determine the length of b, which is around 500.
-# Considering that the 501th in freq_sort may have the same frequency with the 500th, we contain all frequencies greater or equal to 500th.
+# Considering that the 501th in freq_sort may have the same frequency with the 500th
+# We contain all frequencies greater or equal to 500th.
 b_number <- sum(freq_sort >= freq_sort[500]) # sum function helps count the number of TRUE
-# find a threshold number of occurrences at which a word should be included in the set of m ≈ 500 most common words
+
+# a threshold number of occurrences at which a word should be included in the set of m≈500 most common words
 threshold <- freq_sort[b_number]
 
-common_index <- which(freq_vector >= threshold) # find the index of word with occurrences > threshold
+common_index <- which(freq_vector >= threshold) # the index of word with occurrences >= threshold
 b <- b[common_index] # the vector of the most common words
 
+#-------------------------------------------------------------------------------
 
 # step 7
 #In this step, we start to find array T, matrix A and vector S
-new_index <- c(match(a,b)) #return a vector of indices indicating the position of each element of a in b
+
+new_index <- c(match(a,b)) # indicies indicating which element in b each element in a corresponds to
 
 # find array T
-#We use cbind to create a 3 column matrix. 
-#Remove a couple of entries from the end in new_index to form the first column, one from the start and one from the end to form the second column and two from the start for the third column. 
-#Each row indexes a triplet of adjacent words 
+# We use cbind to create a 3 column matrix. 
+# Remove a couple of entries from the end in new_index to form the first column, one from the start and one from the end to form the second column and two from the start for the third column. 
+# Each row indexes a triplet of adjacent words 
 triplet <- cbind(new_index[1:(length(new_index)-2)], new_index[2:(length(new_index)-1)], new_index[3:length(new_index)])
 na_row_index <- which(rowSums(is.na(triplet)) != 0)#remove the rows containing NA
 triplet <- triplet[-na_row_index,]
@@ -102,6 +118,7 @@ for (x in 1:500){
   vector_S[x]=length(which(single==x))
 }
 
+#-------------------------------------------------------------------------------
 
 # step 8
 # In this step, we simulate 50-word sections from our model.
@@ -111,7 +128,7 @@ for (x in 1:500){
 # We can do this by loop, but need to make sure that in the process of looping, 
 # we will not fail to find the next word because the probability is 0.
 # So verify matrix A and array T.
-# try to find whether there is any commom word in b that is never followed by another word in b.
+# For A, we try to find whether there is any commom word in b that is never followed by another word in b.
 test_A <- c(rep(0,500))
 for (i in 1:500){
   test_A[i] <- sum(matrix_A[i,])
@@ -119,40 +136,59 @@ for (i in 1:500){
 test_A_result <- sum(which(test_A == 0))
 test_A_result # equals 0, verifying that every word in b can be followed by another word in b.
 # Our simulation won't be broken when using S and A to generate the first two words.
+
 # a function to simulate the first two words (index)
 simulate_two_words <- function(vector_S, matrix_A){
   two_word <- c(0,0)
-  two_word[1] <- sample(1:500, size = 1, prob = vector_S) # randomly select one number from 1 to 500 based on the prob implied by vector_S
+  two_word[1] <- sample(1:500, size = 1, prob = vector_S) # randomly select one number in [1,500] based on the prob implied by vector_S
   two_word[2] <- sample(1:500, size = 1, prob = matrix_A[two_word[1],])
   two_word
 } 
 
 # However, similar test for array T is difficult.
 # So we add if condition in our code to make sure that our loop can run without interruption.
+
 # a function to simulate the text (index) with default length 50
 simulate_text <- function(vector_S, matrix_A, array_T, length_text=50){
   text <- c(rep(0,length_text))
   text[c(1,2)] <- simulate_two_words(vector_S, matrix_A) # simulate the first two words
-  i <- 3 # a position index which is useful in loop
+  i <- 3 # an index which is the position where we start loop
+  
   repeat{
-    if (sum(array_T[text[i-2],text[i-1],])!=0){ # "not equal to 0" means simulating the following word by array T works well
-      text[i] <- sample(1:500, size = 1, prob = array_T[text[i-2],text[i-1],]) # simulate the i-th word based on the last two word and the corresponding prob
-      i <- i+1 # the next loop is to simulate a word for the next position in text
-    }else{ # else "equal to 0" means we can find the following word by T
-      text[c(i,i+1)] <- simulate_two_words(vector_S, matrix_A) # use S and A again to "restart"
-      i <- i+2 # the next position that needs to be filled is i+2
+    # "!= 0" means simulating the following word by array T works well
+    if (sum(array_T[text[i-2],text[i-1],])!=0){ 
+      # simulate the i-th word based on the last two words and the corresponding prob
+      text[i] <- sample(1:500, size = 1, prob = array_T[text[i-2],text[i-1],]) 
+      # the next loop is to simulate a word for the next position in text
+      i <- i+1} 
+    
+    # else ("= 0") means we cannot find the following word by T. The loop break and we need to "restart"
+    else{ 
+      text[c(i,i+1)] <- simulate_two_words(vector_S, matrix_A) 
+      # the next position that needs to be filled is i+2
+      i <- i+2
     }
-    if (text[length_text]!=0){break} # when the last term in text is filled with "something", the loop should be stopped
+    
+    # when the last term in text is filled with "something", the loop should be stopped
+    if (text[length_text]!=0){break}
   }
   text #return text
 }
-simulate_index_T <- simulate_text(vector_S, matrix_A, array_T) # generate a vector of word index by the above function
-cat(b[simulate_index_T]) # print the simulated words
+
+# generate a vector of word index by the above function
+simulate_index_T <- simulate_text(vector_S, matrix_A, array_T)
+
+# print the simulated words
+cat(b[simulate_index_T]) 
+
+#-------------------------------------------------------------------------------
 
 # step 9
-#Directly simulate 50 words based on probability implied by vector_S， with replacement
+# Directly simulate 50 words based on probability implied by vector_S， with replacement
 simulate_index_S <- sample(1:500, size = 50, prob = vector_S, replace = T)
 cat(b[simulate_index_S])
+
+#-------------------------------------------------------------------------------
 
 # step 10
 #In this step, we would like to replace the words that most often start with a capital letter with an uppercase initial form, 
@@ -175,6 +211,7 @@ capital_index <- match(a_with_capital,capital)
 #It is convenient for us to subsequently use the tabulate function to calculate the total number of occurrences in the text 
 #of all words that have had their initial letter be capital. 
 lower_capital_index <- match(a, tolower(capital))
+lower_capital_index
 
 freq_capital <- tabulate(capital_index) #The total number of occurrences in the text of all words that have had their initial letter be capital.
 freq_lower_capital <- tabulate(lower_capital_index)#Total frequency of the word which can start with both lowercase and capital letters.
@@ -196,3 +233,5 @@ replace_index <- which(!is.na(match_index))
 # which is same with most often start with capital letter to the form that start with capital letter
 b_with_capital[replace_index] <- most_often[match_index[replace_index]]
 cat(b_with_capital[simulate_index_T])
+
+

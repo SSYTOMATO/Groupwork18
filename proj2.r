59c12c90c740fb012a5ed795094af8436e783946
Mengyao41 <s2049018@ed.ac.uk>
2022-10-13 03:48:24 +0100

    created the file for project 2 and added some original comments

---
+++ proj2.r
@@ -0,0 +1,20 @@
+# Group 18
+# Member: Shangyun Sun(S2330859); Ziyi Gu(S2307232); Mengyao Zhang(S2049018).
+
+# Github repo: https://github.com/SSYTOMATO/Groupwork18.git
+
+# Shangyun Sun (some contributions).
+# Ziyi Gu (some contributions).
+# Mengyao Zhang (some contributions).
+# Each of us double-checked and optimized others' codes.  
+# Everyone undertook roughly 1/3 of the work.
+
+#-------------------------------------------------------------------------------
+# Overview
+
+
+
+
+
+
+#-------------------------------------------------------------------------------

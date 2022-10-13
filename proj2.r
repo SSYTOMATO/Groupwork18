27b3ad9d21c8c8f13138b6370a7af65466ea0ba0
SSYTOMATO <s2330859@ed.ac.uk>
2022-10-13 22:49:55 +0800

    rough codes of function Pone

--- proj2.r
+++ proj2.r
@@ -18,3 +18,34 @@
 
 
 #-------------------------------------------------------------------------------
+Unit <- function(n,k,first,box){
+  box_index <- first
+  success <- 0
+  for (i in 1:n){
+    if (k != box[box_index]){
+      box_index <- box[box_index]
+    } else {
+      success <- 1
+      break
+    }
+  }
+  success
+}
+
+Pone <- function(n,k,strategy,nreps){
+  for (rep in 1:nreps){
+    box <- sample(1:2*n,2*n)
+    success <- 0
+    # strategy 1
+    success <- success + Unit(n,k,k,box)
+    # strategy 2
+    first <- sample(1:2*n,1)
+    success <- success + Unit(n,k,first,box)
+    # strategy 3
+    choice <-sample(1:2*n,n)
+    success <- success + k %in% choice
+  }
+  success/nreps
+}
+  
+

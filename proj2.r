c736510e550e8558b90bf265508d91431bcdf6e7
Mengyao41 <s2049018@ed.ac.uk>
2022-10-13 19:45:40 +0100

    modified function Pall whose examples are more likely to be correct, but still need check

--- proj2.r
+++ proj2.r
@@ -68,16 +68,57 @@ Pone <- function(n,k,strategy,nreps=10000){
 
 
 # function Pall
+# Similar as function Pone, but consider joint probability, that is,
+# in each simulation, find whether all prisoners can success.
+# Simulate nreps times, calculate the probability.
+
 Pall <- function(n,strategy,nreps=10000){
-  random_number <- sample(1:10000,1)
-  prob <- 1
-  list1 <- rep(0,2*n)
-  for (k in 1:2*n){
-    set.seed(random_number)
-    prob <- prob * Pone(n,k,strategy,nreps)
-    list1[k] <- prob
+  
+  success_num <- 0
+  
+  # strategy 1
+  if (strategy == 1){
+    
+    for (rep in 1:nreps){
+      success <- 1
+      box <- sample(2*n,2*n)
+      for (k in 1:(2*n)){
+        success <- success * Unit(n,k,k,box)
+        if (success == 0){break}
+      }
+    success_num <- success_num + success  
+    }
   }
-  list(list1, prob)
+  
+  # strategy 2
+  else if (strategy == 2){
+    
+    for (rep in 1:nreps){
+      success <- 1
+      box <- sample(2*n,2*n)
+      for (k in 1:(2*n)){
+        first <- sample(2*n,1)
+        success <- success * Unit(n,k,first,box)
+        if (success == 0){break}
+      }
+      success_num <- success_num + success  
+    }
+  }
+  
+  # strategy 3
+  #if(strategy == 3)
+  else {
+    for (rep in 1:nreps){
+      success <- 1
+      for (k in 1:(2*n)){
+        choice <-sample(2*n,n)
+        success <- success * k %in% choice
+        if (success == 0){break}
+      }
+      success_num <- success_num + success
+    }
+  } 
+  success_num/nreps
 }
 
 
@@ -87,6 +128,7 @@ for (strategy in 1:3){
   matrix_n5[strategy,1] <- Pone(5,1,strategy)
   matrix_n5[strategy,2] <- Pall(5,strategy)
 }
+matrix_n5
 
 # n = 50 matrix, row-strategy, column-indi/joint
 matrix_n50 <- matrix(0,3,2)
@@ -94,18 +136,4 @@ for (strategy in 1:3){
   matrix_n50[strategy,1] <- Pone(50,1,strategy)
   matrix_n50[strategy,2] <- Pall(50,strategy)
 }
-
-Pone(50,1,3)
-Pall(50,2)
-
-random_number <- sample(1:10000,1)
-prob <- 1
-for (k in 1:100){
-  set.seed(random_number)
-  prob <- prob * Pone(50,k,3)
-  print(prob)
-}
-
-
-
-
+matrix_n50

7b926c054831fc5c11a3dbd48fbc62ee4005f131
Mengyao41 <s2049018@ed.ac.uk>
2022-10-19 12:08:56 +0100

    little changes of format

--- proj2.r
+++ proj2.r
@@ -36,12 +36,13 @@
 
 #-------------------------------------------------------------------------------
 
-# 
 # a pre function
+
 Unit <- function(n,k,first,box){
+  
 # Function unit is a pre-function for other functions later, which is used to 
 # determine whether prisoners can successful within n times.
-# 
+
 # Input: n (the maximum number that boxes can be opened)
 #        k (the index of the first prisoner)
 #        first (the index of the first box we chosen)
@@ -50,15 +51,16 @@ Unit <- function(n,k,first,box){
 # Output: an integer which is marked as 1 when it success, otherwise marked as 0
   
   box_index <- first
-  success <- 0 # 1 represents success, 0 represents failure
-  for (i in 1:n){ # loop for n times, because maximum number of opened box is n
-    if (k != box[box_index]){ # if k is not the prisoner's number,
-      box_index <- box[box_index]  # prisoner goes to box number k, 
-                                   # open it and repeat the process.
+  success <- 0  # 1 represents success, 0 represents failure
+  
+  for (i in 1:n){  # loop for n times, because maximum number of opened box is n
+    if (k != box[box_index]){  # if k is not the prisoner's number
+      box_index <- box[box_index]   # prisoner goes to box number k
+                                    # open it and repeat the process
       
     } else {
-      success <- 1 # when the card number equals to prisoner's number, 
-      # the experiment is successful, we set success as 1, and stop loop.
+      success <- 1  # when the card number equals to prisoner's number, the
+                    # experiment is successful; set success as 1, and stop loop
       break
     }
   }
@@ -67,23 +69,26 @@ Unit <- function(n,k,first,box){
 
 #-------------------------------------------------------------------------------
 
-
 # function Pone
+
 Pone <- function(n,k,strategy,nreps=10000){
+  
 # Function pone is used to estimate probability of single prisoners success in  
 # each strategy within nreps experiments. 
+  
 # Input: n (used to calculate the number of prisoners (2*n) and box (2*n))
 #        k (the index of the first prisoner)
 #        strategy (can equal to 1,2,3, represents 3 strategies respectively)
 #        nreps (the amount of experiments, which is defaulted to 10000)
 # Output: a float which is the probability 1 prisoner can find his or her number
+  
   success <- 0
     
     # strategy 1
-  if (strategy == 1){ # decide to use which stategy
-    for (rep in 1:nreps){ # loop for nreps experiments
-      box <- sample(2*n,2*n) # random simulate the card's number in boxes
-      success <- success + Unit(n,k,k,box) # calculate the number of success
+  if (strategy == 1){         # decide to use which stategy
+    for (rep in 1:nreps){     # loop for nreps experiments
+      box <- sample(2*n,2*n)  # random simulate the card's number in boxes
+      success <- success + Unit(n,k,k,box)  # calculate the number of success
     }
   }
     
@@ -91,7 +96,7 @@ Pone <- function(n,k,strategy,nreps=10000){
   else if (strategy == 2){
     for (rep in 1:nreps){
       box <- sample(2*n,2*n)
-      first <- sample(2*n,1) # start the experiment with random selected box
+      first <- sample(2*n,1)  # start the experiment with random selected box
       success <- success + Unit(n,k,first,box)
     }
   }
@@ -99,34 +104,35 @@ Pone <- function(n,k,strategy,nreps=10000){
     # strategy 3
   else if(strategy == 3){
     for (rep in 1:nreps){
-      choice <-sample(2*n,n) # open n boxes at random
-      success <- success + k %in% choice # if k is in those opened box's 
-      # card number, we think experiment success.
+      choice <-sample(2*n,n)  # open n boxes at random
+      success <- success + k %in% choice  # if k is in those opened box's card
+                                          # number, we think experiment success
     }
   } 
-  success/nreps # success probability = (the number of simulations with
-                # result of success) / (the total number of simulations)
+  success/nreps  # success probability = (the number of simulations with
+                 # result of success) / (the total number of simulations)
 }
 
 #-------------------------------------------------------------------------------
 
+# function Pall
+
 Pall <- function(n,strategy,nreps=10000){
+
+# Estimate the probability of all prisoners finding their number
+# By stochastic simulation
   
-  # Estimate the probability of all prisoners finding their number
-  # By stochastic simulation
-  
-  # Input: n (the maximum number of boxes one prisoner can open, also, 2n is
-  #           the number of prisoners, boxes and cards)
-  #        strategy (take values of 1, 2, 3,
-  #                  corresponding to strategies stated above)
-  #        nreps (the number of replicate simulations to run in order to
-  #               estimate the probability, with default value 10000)
-  # Output: a float which is the probability of all prisoners finding their number
+# Input: n (the maximum number of boxes one prisoner can open, also, 2n is
+#           the number of prisoners, boxes and cards)
+#        strategy (take values of 1, 2, 3,
+#                  corresponding to strategies stated above)
+#        nreps (the number of replicate simulations to run in order to
+#               estimate the probability, with default value 10000)
+# Output: a float which is the probability of all prisoners finding their number
   
   success_num <- 0  # the times of simulations with result of success
   
   # strategy 1
-  
   if (strategy == 1){
     for (rep in 1:nreps){     # loop for every simulation
       success <- 1            # 1 represents success, 0 represents failure
@@ -142,7 +148,6 @@ Pall <- function(n,strategy,nreps=10000){
   }
   
   # strategy 2
-  
   else if (strategy == 2){      # similar as the strategy 1
     for (rep in 1:nreps){
       success <- 1
@@ -157,7 +162,6 @@ Pall <- function(n,strategy,nreps=10000){
   }
   
   # strategy 3
-
   else {
     for (rep in 1:nreps){
       success <- 1
@@ -182,6 +186,7 @@ Pall <- function(n,strategy,nreps=10000){
 # row i is the result of strategy i
 # column 1 is individual probability and column 2 is joint success probability
 
+# n = 5
 matrix_n5 <- matrix(0,3,2)
 for (strategy in 1:3){
   matrix_n5[strategy,1] <- Pone(5,1,strategy)
@@ -189,7 +194,7 @@ for (strategy in 1:3){
 }
 matrix_n5
 
-# n = 50 matrix, row-strategy, column-indi/joint
+# n = 50
 matrix_n50 <- matrix(0,3,2)
 for (strategy in 1:3){
   matrix_n50[strategy,1] <- Pone(50,1,strategy)
@@ -205,8 +210,9 @@ matrix_n50
 # but Joint success probability of strategy 2&3 are close to 0.
 # The result is more obvious when n becomes larger.
 
-
 #-------------------------------------------------------------------------------
+
+# function dloop
 
 dloop <- function(n,nreps=10000){
   
@@ -220,21 +226,21 @@ dloop <- function(n,nreps=10000){
 # Output: a 2n vector of probabilities of each loop length occurring at
 #         least once in a random shuffling of cards to boxes
   
-    freq <- rep(0,2*n)   # number of each loop length occurring 
-  for (reps in 1:nreps){ # loop for nreps simulations
-    u <- sample(2*n,2*n) # generate cards number in boxes
-    u_index <- 1:(2*n)   # a vector of box number
-    len_list <- c()      # a vector to store the loop length
-    while (length(u_index) != 0){ # while there are still boxes to be opened
+    freq <- rep(0,2*n)    # number of each loop length occurring 
+  for (reps in 1:nreps){  # loop for nreps simulations
+    u <- sample(2*n,2*n)  # generate cards number in boxes
+    u_index <- 1:(2*n)    # a vector of box number
+    len_list <- c()       # a vector to store the loop length
+    while (length(u_index) != 0){  # while there are still boxes to be opened
       first <- u_index[1] # the first box number 
       k <- first          # box number with initial value equal to first
       ring <- k           # loop starting with number k
       length <- 1         # loop length with initial value 1
-      while (u[k] != first){ # while card number in box k is not equal to
-                             # the first box number
-        k <- u[k]            # let box number equal to the card number 
-        length <- length + 1 # loop length increases 1
-        ring <- c(ring,k)    # add the new box number to the loop
+      while (u[k] != first){  # while card number in box k is not equal to
+                              # the first box number
+        k <- u[k]             # let box number equal to the card number 
+        length <- length + 1  # loop length increases 1
+        ring <- c(ring,k)     # add the new box number to the loop
       }
       u_index <- u_index[-which(u_index %in% ring)] # remove the box number used
       len_list <- c(len_list,length)                # store this loop length
@@ -242,9 +248,12 @@ dloop <- function(n,nreps=10000){
     # add one to the corresponding loop length frequency 
     freq[which(1:(2*n) %in% len_list)] <- freq[which(1:(2*n) %in% len_list)] + 1
   }
-  freq/nreps # probability = (the number of simulations with result of freq)
-             #  / (the total number of simulations)
+  freq/nreps # probability = (the number of simulations with result of freq) / 
+             # (the total number of simulations)
 }
+
+
+# some changes needed
 
 test <- dloop(50)
 test
@@ -258,6 +267,7 @@ x<-seq(1,100)
 
 barplot(dloop(50),names=x,xlab = 'Loop length',ylab = 'Probability',ylim = c(0,0.7))
 lines(x,dloop(50),type="l",col="blue")
+
 # Analysis:
 # When we perform the experiment with n=50. We can see from the graph 
 # that a loop of length 1 has a higher probability compared to the other lengths.

725780b7685f67fdd3cbfc8db8b56bdfafc87209
SSYTOMATO <s2330859@ed.ac.uk>
2022-11-14 02:03:28 +0800

    new group project

---
+++ proj4.r
@@ -0,0 +1,7 @@
+# Group 18
+# Member: Shangyun Sun(S2330859); Ziyi Gu(S2307232); Mengyao Zhang(S2049018).
+
+# Github repo: https://github.com/SSYTOMATO/Groupwork18.git
+
+
+#-------------------------------------------------------------------------------

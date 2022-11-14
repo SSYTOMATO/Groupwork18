385e23c49821ea8590babbd6c37b79173fcd3c81
Mengyao41 <s2049018@ed.ac.uk>
2022-11-14 16:14:45 +0000

    some warnings added

--- proj4.r
+++ proj4.r
@@ -25,27 +25,31 @@ hessain <- function(theta,grad,...,eps=1e-6){
 newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,
                  maxit=100,max.half=20,eps=1e-6){
   k <- 0
+  func_k <- func(theta,...)
+  grad_k <- grad(theta,...)
+  if (is.NULL(hess)){
+    hess_k <- hessian(theta,grad,...)
+  }else{hess_k <- hess(theta,...)}
   
-  while(k <= maxit){
+  if (sum(is.finite(func_k))+sum(is.finite(grad_k)) != 
+      length(func_k)+length(grad_k)){
+    stop('the objective or derivatives are not finite at the initial theta')
+  }
 
-    func_k <- func(theta,...)
-    grad_k <- grad(theta,...)
-    if (is.NULL(hess)){
-      hess_k <- hessian(theta,grad,...)
-    }else{hess_k <- hess(theta,...)}
     
-    if (k == 0){
-      ## warnings 1 needs adding
+  while(k <= maxit){
+    
+    # judge whether theta is just the parameter we want to find
+    if (abs(grad_k) < tol*abs(func_k)+fscale){
+      break
+    } else if (k==maxit){
+      stop('maxit is reached without convergence')
     }
     
-    ## judge whether theta is just the parameter we want to find
-    ## if it is, break
-    ## if it is not and k = maxit, warning 3
     
+    ## remaining1: whether hess_k is positive definite,if not, perturb it.
     
-    ## whether hess_k is positive definite,if not, perturb it.
-    
-    ## find the step(delta) by delta= -(hess_k)^(-1) %*% grad_k
+    ## remaining2: find the step(delta) by delta= -(hess_k)^(-1) %*% grad_k
     
     if (func(theta+delta) >= func_k){
       for (i in 1:max.half){
@@ -53,53 +57,26 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,
         if (func(theta+delta) < func_k){break}
       }
       if (func(theta+delta) >= func_k){
-        # warning 2 needs adding
+        stop('the step fails to reduce the objective with max.half halvings')
       }
     }
     
     theta <- theta + delta
     k <- k+1
+    func_k <- func(theta,...)
+    grad_k <- grad(theta,...)
+    if (is.NULL(hess)){
+      hess_k <- hessian(theta,grad,...)
+    }else{hess_k <- hess(theta,...)}
     
   }
   
-  # for final theta, check whether hess is positive definite, warning 4
+  # remaining3: check whether final hess is positive definite, warning 4
   # the inverse of the Hessian matrix?
   
   
  c(f=func_k, theta=theta, iter=k, g=grad_k, Hi) 
 }  
   
-  
-  
-  
-  
-'''
-  
-  function_value <- func(theta,...)#function value at initial parameter values
-  gll <- c(0)
-  number_not_ruduce = 0
-  
-  for (i in 1:maxit+1) {
-    Hfd <- matrix(0,length(theta[1]),length(theta[1])) # empty hessain matrix
-    
-    gll[i] <- grad(theta[i]) #find the gradient of theta[i]
-    for(i in 1:length(theta[1])){
-      # use gll[i] to find the second derivative
-      th1 <- theta
-      th1[i] <- th1[i] + eps
-      gll1 <- func(th1)
-      Hfd[i,] <- (gll1 - gll0)/eps
-    }
-    theta[i+1] <- theta[i] - gll[i]/Hfd #find a new parameter matrix
-    if(theta[i+1] >= theta[i]){
-      number_not_ruduce = number_not_ruduce +1
-    }
-    i = i+1
-    
-    
-   }
-  
-'''  
-    
   
 
